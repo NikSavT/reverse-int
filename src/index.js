493a2dac9e802328cd83b36df7f4c67537b15cbd
NikSavT <hickhickert@gmail.com>
2023-01-14 00:54:07 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+  let reverseNum = '',
+    numString = n >= 0 ? String(n) : String(-n);
+
+  for (i = 0; i < numString.length; i++) {
+    reverseNum = numString[i] + reverseNum;
+  }
+
+  return +reverseNum;
+};
+
+// console.log(module.exports(261));
